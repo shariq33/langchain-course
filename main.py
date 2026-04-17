7e4fe06f60a497d4509c577eab4bfb4690d4a51f
mnakhawa-JNPR <mnakhawa@juniper.net>
2026-04-17 12:31:11 -0400

    environment setup

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from dotenv import load_dotenv
 
 load_dotenv()
-import os
 
 
 def main():
     print("Hello from langchain-course!")
-    print(os.environ.get("GEMINI_API_KEY"))
-    print(os.environ.get("OLLAMA_API_KEY"))
-
+    
 
 if __name__ == "__main__":
     main()
