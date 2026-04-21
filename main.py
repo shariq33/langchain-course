39cee5ca93db613741a5139a22ae6c7515637d24
mnakhawa-JNPR <mnakhawa@juniper.net>
2026-04-21 19:24:00 -0400

    env setup

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from dotenv import load_dotenv
 
 load_dotenv()
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
 
 
 def main():
     print("Hello from langchain-course!")
-    
+
 
 if __name__ == "__main__":
     main()
