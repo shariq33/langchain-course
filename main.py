550fbeb91d3f1d2579e6f3f6432616e4e019bb2d
mnakhawa-JNPR <mnakhawa@juniper.net>
2026-04-22 14:08:15 -0400

    intro to search agents

--- main.py
+++ main.py
@@ -1,47 +1,45 @@
-from typing import List
-
-from pydantic import BaseModel, Field
 from dotenv import load_dotenv
-
 load_dotenv()
+import os
+
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from tavily import TavilyClient
 from langchain_tavily import TavilySearch
 
-
-class Source(BaseModel):
-    """Schema for a source used by the agent"""
-
-    url: str = Field(description="The URL of the source")
+load_dotenv()
 
 
-class AgentResponse(BaseModel):
-    """Schema for agent response with answer and sources"""
 
-    answer: str = Field(description="Thr agent's answer to the query")
-    sources: List[Source] = Field(
-        default_factory=list, description="List of sources used to generate the answer"
-    )
+tavily_key = os.getenv("TAVILY_API_KEY")
+print(tavily_key)
+tavily = TavilyClient(api_key=tavily_key)
+
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+        query: The query to search for
+    Returns:
+        The search result
+    """
+    return tavily.search(query=query, max_results=2)
 
 
-llm = ChatOpenAI(model="gpt-5")
+#llm = ChatOpenAI(model="gpt-5")
+llm = ChatOllama(temperature=0, model="mistral:latest")
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
+agent = create_agent(model=llm,tools=tools)
 
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke(
-        {
-            "messages": HumanMessage(
-                content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?"
-            )
-        }
-    )
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
     print(result)
-
 
 if __name__ == "__main__":
     main()
